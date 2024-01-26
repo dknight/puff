a49a93d8e16fa56d053be2652a45bbf497938969
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-01-26 02:34:14 +0200

    ci: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ npm run build
 current=$( node -e "console.log(require('./package.json').version)"  | tr -d "\n" ) 
 next=$( npm version $type | tr -d "\n" ) 
 
+git add .
+git commit -m "Release: v$next"
+
 git push origin "$next"
 git push origin main
 
