c861c13fc2b80cd9c0d4b6d12b2d9cf03899526b
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-01-26 02:33:30 +0200

    ci: added build command to deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ if [ -z "$type" ]; then
   type="patch"
 fi
 
+npm run build
+
 current=$( node -e "console.log(require('./package.json').version)"  | tr -d "\n" ) 
 next=$( npm version $type | tr -d "\n" ) 
 
